c96fd1f21c254134116fd8e9080e61daceb0015f
Anurag Bhaskar <anurag@Anurag-Bhaskars-2020-MacBook-Pro.local>
2020-07-16 23:46:58 +0530

    Integrated friendly routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'About-me', to: 'pages#About'
   get 'Contact', to: 'pages#Contact'
   resources :blogs
+  
   # Setting the homepage
   root to: 'pages#Home'
 end
