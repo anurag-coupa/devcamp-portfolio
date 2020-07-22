77940f9860097836f2896650d3acc829bfb9453d
Anurag Bhaskar <anurag@Anurag-Bhaskars-2020-MacBook-Pro.local>
2020-07-23 02:10:46 +0530

    Integrated newly made gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    AnuragViewTool::Renderer.copyright 'Anurag Bhaskar', 'All Rights Reserved'
+  end
 end
