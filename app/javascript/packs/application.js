1caba2108dde01b7b2d54ef71801b83035cc44a5
Anurag Bhaskar <anurag@Anurag-Bhaskars-2020-MacBook-Pro.local>
2020-07-26 23:14:15 +0530

    Integrated image background for app layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,9 +13,9 @@ require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
-require('lib/following');
-require('trix');
-require('@rails/actiontext');
+//= require('lib/following');
+//= require('trix');
+//= require('@rails/actiontext');
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -24,6 +24,5 @@ require('@rails/actiontext');
 // const imagePath = (name) => images(name, true)
 //= require popper
 //= require bootstrap
-//= require rails-ujs
 
 import "controllers"
