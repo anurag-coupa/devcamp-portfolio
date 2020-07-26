68e12af0f58de0c3d15a610ee939e9d435cb0d1d
Anurag Bhaskar <anurag@Anurag-Bhaskars-2020-MacBook-Pro.local>
2020-07-26 21:49:22 +0530

    Implemented persistent position updates for drag and drop interfaces

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -8,22 +8,30 @@
 // </div>
 
 import { Controller } from "stimulus"
-import Sortable from "sortablejs"
-
+import Sortable from 'sortablejs'
+import Rails from '@rails/ujs'
+ 
 export default class extends Controller {
+  static targets = [ "output" ]
+ 
   connect() {
+    // this.outputTarget.textContent = 'Hello, Stimulus!'
+ 
     this.sortable = Sortable.create(this.element, {
       onEnd: this.end.bind(this)
     })
   }
-
-  end(event){
-    let id = event.item.dataset.id
+ 
+  end(event) {
+    console.log(event)
     let data = new FormData()
-    data.append("position", event.newIndex + 1)
-
+    let id = event.item.dataset.id
+ 
+    data.append('position', event.newIndex + 1)
+ 
     Rails.ajax({
-      url: this.data.get("url").replace(":id", id),
+      // url: this.data.get('url').replace(':id', id),
+      url: `/portfolios/${id}/move`,
       type: 'PATCH',
       data: data
     })
